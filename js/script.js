375d772b44f4ae673824b2f594e06701e6317ed1
RSabiroff <74386277+RSabiroff@users.noreply.github.com>
2023-10-10 01:13:19 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
 'use strict';
 
+// document.addEventListener("pointermove", function (e) {
+// 	console.log(e.screenX);
+// });
+
 let imgs = document.getElementsByTagName('img');
+
 for (let img of imgs) {
 	img.setAttribute("draggable", "false");
 }
 
+
 let grade = document.getElementsByClassName('grade');
 
 let x1;
-
 let x2;
+let x3;
 
 for (let i of grade) {
-	i.addEventListener("touchstart", function (e) {
-		e.preventDefault();
-		e.stopPropagation();
-
+	i.addEventListener("pointerdown", function (e) {
 		x1 = e.clientX;
 	});
 
-	i.addEventListener("touchmove", function (e) {
-		e.preventDefault();
-		e.stopPropagation();
+	i.addEventListener("pointermove", function (e) {
 		x2 = e.clientX;
-		let x3 = x2 - x1;
+		x3 = x2 - x1;
 		ID_test.style.marginLeft = x3 + "px";
 	});
 }
