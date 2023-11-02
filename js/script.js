39c2ba5c9bd7bdf83386e4b35e4f359c1213aadb
RSabiroff <74386277+RSabiroff@users.noreply.github.com>
2023-11-02 11:56:57 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ for (let img of imgs) {
 	img.setAttribute("draggable", "false");
 	
 	img.onclick = function (e) {
+		document.getElementById('ID_bigImage').setAttribute('srcset', img.getAttribute('srcset'));
 		document.getElementById('ID_bigImage').setAttribute('src', img.getAttribute('src'));
 
 		document.getElementById('ID_bigImage-wrapper').style.display = 'flex';
